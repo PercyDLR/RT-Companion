12673adc6a8dd44fa5f0a7b018114cfefb080764
PercyDLR <a20192265@pucp.edu.pe>
2023-10-02 10:43:55 -0500

    Se creó la ventana de cuenta. Se configuró el esquema de colores.

---
+++ app/src/main/java/pe/edu/pucp/rtcompanion/CuentaUsuarioActivity.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.rtcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuentaUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_usuario);
+    }
+}
